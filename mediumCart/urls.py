850695f0a5beabfc7b2034932ca2bf251f0bccbc
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-08-19 20:57:29 +0530

    Added admin

--- mediumCart/urls.py
+++ mediumCart/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("", include("home.urls"), name="Home"),
     path("blog", include("blog.urls"), name="Blog"),
     path("checkout", include("checkout.urls"), name="Checkout"),
