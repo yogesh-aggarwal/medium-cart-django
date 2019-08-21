55d92eb905d2926f07b8ce137952ed878705b3e6
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-08-21 23:34:17 +0530

    Added POST request condition for contact

--- mediumCart/views.py
+++ mediumCart/views.py
@@ -5,4 +5,9 @@ def about(request):
     return render(request, "about/index.html")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("fullName")
+        email = request.POST.get("email")
+        issue = request.POST.get("issue")
+        # Now insert them into the database "contact.sqlite3"
     return render(request, "contact/index.html")
