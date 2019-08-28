0b44bf6c58c9958f4bbb8f200363904c05432f6e
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-08-28 19:12:20 +0530

    Declared search as an end-point for searching (routed)

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:query>", views.EndPoint.index, name="Search"),
+    path("", views.EndPoint.index, name="Search"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from sql_tools import sqlite
 
 
@@ -7,18 +8,20 @@ class EndPoint:
         pass
 
     @staticmethod
-    def index(request, query):
+    def index(request):
+        query = request.GET.get("query")
+        print(query)
+
         sqlite.connect(
             "/media/yogesh/Development/Python/django/mediumCart/database/data/products.sqlite3",
             raiseError=False,
         )
 
+        result = []
         # Perform search functionality here
 
-        result = []
-
-        attributes = {
-            "query": query,
-            "result": result
-        }
-        return render(request, "search/index.html", attributes)
+        attributes = {"query": query, "result": result}
+        if query is not "":
+            return render(request, "search/index.html", attributes)
+        else:
+            return HttpResponseRedirect("/")
